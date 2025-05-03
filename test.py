7e2c50d921367542b4165793ac0180eb1c120878
Amartya Singh <119071717+Beingstupid4me@users.noreply.github.com>
2025-05-03 07:04:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 class QwenChatbot:
-    def __init__(self, model_name="Qwen/Qwen3-1.7B"):
+    def __init__(self, model_name="../Qwen3-1.7B"):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.history = []
